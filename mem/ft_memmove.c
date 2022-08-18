a725b6fb7f4bc38f07b5ff6ac534ba47dcce9eeb
Tharunthorn Musik <tharunthornmusik@Tharunthorns-MacBook-Air.local>
2022-08-18 13:00:40 +0700

    ft_memmove

---
+++ mem/ft_memmove.c
@@ -0,0 +1,50 @@
+
+/**
+ * TODO Fix below
+ * ! Missing 42Header
+ * ! Missing Norm
+ * ? Param unsigned int need to change to size_t
+ */
+
+void *ft_memmove(void *dst, const void *src, unsigned int len)
+{
+    unsigned char *dsttemp;
+    unsigned char *lastdst;
+    const unsigned char *srctemp;
+    const unsigned char *lastsrc;
+
+    dsttemp = dst;
+    srctemp = src;
+    if (dsttemp < srctemp)
+        while (len--)
+            *dsttemp++ = *srctemp++;
+    else
+    {
+        lastdst = dsttemp + (len - 1);
+        lastsrc = srctemp + (len - 1);
+        while (len--)
+            *lastdst-- = *lastsrc--;
+    }
+    return (dst);
+}
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[] = "Geeks"; // Array of size 100
+    char str2[] = "Quiz";  // Array of size 5
+
+    puts("str1 before memmove ");
+    puts(str1);
+
+    // Copies contents of str2 to sr1
+    ft_memmove(str1, str2, sizeof(str2));
+
+    puts("\nstr1 after memmove ");
+    puts(str1);
+    return 0;
+}
+*/
