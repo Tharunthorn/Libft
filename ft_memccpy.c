d31a7c72f6bba5af261d242dbef2b9dc09e7816e
tharunthorn <tharunthornmusik1446@gmail.com>
2022-08-20 13:38:21 +0700

    NormFix

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,40 +6,34 @@
 /*   By: thmusik <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 00:33:00 by thmusik           #+#    #+#             */
-/*   Updated: 2022/08/20 12:32:05 by thmusik          ###   ########.fr       */
+/*   Updated: 2022/08/20 13:22:18 by thmusik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/**
- * TODO Fix below
- * ! Missing Norm check
- */
-
 #include "libft.h"
 
-void *ft_memccpy(void *restrict dst, const void *restrict src,
-                 int terminatebyte, size_t bytelenght)
+void	*ft_memccpy(void *restrict dst, const void *restrict src,
+		int terminatebyte, size_t bytelenght)
 {
-    size_t lenght;
+	size_t	lenght;
 
-    lenght = 0;
-    while (lenght < bytelenght)
-    {
-        *(unsigned char *restrict)dst = *(unsigned char *restrict)src;
-        if (*(unsigned char *restrict)src == terminatebyte)
-        {
-            dst++;
-            break;
-        }
-        dst++;
-        src++;
-    }
-    while (lenght < bytelenght)
-    {
-        *(unsigned char *restrict)dst = '\0';
-        lenght++;
-    }
-    return (dst);
+	lenght = 0;
+	while (lenght < bytelenght)
+	{
+		*(unsigned char *restrict)dst = *(unsigned char *restrict)src;
+		if (*(unsigned char *restrict)src == terminatebyte)
+		{
+			dst++;
+			break ;
+		}
+		dst++;
+		src++;
+	}
+	while (lenght < bytelenght)
+	{
+		*(unsigned char *restrict)dst = '\0';
+		lenght++;
+	}
+	return (dst);
 }
 
 /*
