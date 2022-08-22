786039f1fbfd0968617f83df9e0414234028b85c
Tharunthorn Musik <tharunthornmusik@Tharunthorns-MacBook-Air.local>
2022-08-22 10:59:07 +0700

    libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: thmusik <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 08:11:25 by thmusik           #+#    #+#             */
-/*   Updated: 2022/08/22 09:54:05 by thmusik          ###   ########.fr       */
+/*   Updated: 2022/08/22 10:58:36 by thmusik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int ft_isalpha(int c);
 int ft_isascii(int c);
 int ft_isdigit(int c);
 int ft_isprint(int c);
+char *ft_itoa(int n);
 void *ft_memccpy(void *restrict dst, const void *restrict src,
 				 int terminatebyte, size_t bytelenght);
 void *ft_memchr(const void *s, int c, size_t n);
@@ -35,6 +36,7 @@ void ft_putchar_fd(char c, int fd);
 void ft_putendl_fd(char *s, int fd);
 void ft_putnbr_fd(int n, int fd);
 void ft_putstr_fd(char *s, int fd);
+char **ft_split(char const *s, char c);
 char *ft_strchr(const char *s, int c);
 char *ft_strdup(const char *str);
 char *ft_strjoin(char const *s1, char const *s2);
@@ -43,6 +45,7 @@ size_t strlcat(char *restrict dst, const char *restrict src,
 size_t ft_strlcpy(char *restrict dst, const char *restrict src,
 				  size_t dstsize);
 size_t ft_strlen(const char *str);
+char *ft_strmapi(const char *s, char (*f)(unsigned int, char));
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 char *ft_strnstr(const char *haystack, const char *needle, size_t len);
 char *ft_strrchr(const char *s, int c);
