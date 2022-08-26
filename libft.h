12e7f0a78865c25cac07b2a5e5fdfae88d68430a
Tharunthorn Musik <tharunthornmusik@Tharunthorns-MacBook-Air.local>
2022-08-26 08:57:02 +0700

    t_list

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: thmusik <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 08:11:25 by thmusik           #+#    #+#             */
-/*   Updated: 2022/08/26 08:43:04 by thmusik          ###   ########.fr       */
+/*   Updated: 2022/08/26 08:55:28 by thmusik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,74 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 int		ft_atoi(const char *str);
+
 void	ft_bzero(void *s, size_t n);
+
 void	*ft_calloc(unsigned long count, unsigned long size);
+
 int		ft_isalnum(int c);
+
 int		ft_isalpha(int c);
+
 int		ft_isascii(int c);
+
 int		ft_isdigit(int c);
+
 int		ft_isprint(int c);
+
 char	*ft_itoa(int n);
+
 void	*ft_memchr(const void *s, int c, size_t n);
+
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
 void	*ft_memcpy(void *dst, const void *src, size_t bytesize);
+
 void	*ft_memmove(void *dst, const void *src, size_t len);
+
 void	*ft_memset(void *adress, int value, size_t bytelenght);
+
 void	ft_putchar_fd(char c, int fd);
+
 void	ft_putendl_fd(char *s, int fd);
+
 void	ft_putnbr_fd(int n, int fd);
+
 void	ft_putstr_fd(char *s, int fd);
+
 char	*ft_strchr(const char *s, int c);
+
 char	*ft_strdup(const char *str);
+
 char	*ft_strjoin(char const *s1, char const *s2);
+
 size_t	strlcat(char *restrict dst, const char *restrict src,
 			size_t size);
+
 size_t	ft_strlcpy(char *restrict dst, const char *restrict src,
 			size_t dstsize);
+
 size_t	ft_strlen(const char *str);
+
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
 char	*ft_strrchr(const char *s, int c);
+
 char	*ft_strtrim(char const *s1, char const *set);
+
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+
 int		ft_tolower(int c);
+
 int		ft_toupper(int c);
 
 #endif
