596262f6300ce814a0c73d89974d2df2d6424736
Tharunthorn Musik <thmusik@z1t4c3.42bangkok.com>
2022-09-17 15:30:57 +0700

    lstaddback

--- libft.h
+++ libft.h
@@ -40,6 +40,8 @@ int		ft_isprint(int c);
 
 char	*ft_itoa(int n);
 
+void	ft_lstadd_back(t_list **lst, t_list *new);
+
 void	ft_lstadd_front(t_list **lst, t_list *new);
 
 t_list	*ft_lstlast(t_list *lst);
